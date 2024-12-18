9939fb0482091f51f7aa46345fd61128cf209328
lgk-bsw <lgk@patorg.de>
2024-12-18 12:06:15 +0100

    Added side-layout

---
+++ src/docs/components/DemoCode.tsx
@@ -0,0 +1,39 @@
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { synthwave84 as syntaxHighlighterStyle } from "react-syntax-highlighter/dist/esm/styles/prism"
+import prettier from "prettier/standalone"
+import parserHtml from "prettier/parser-html"
+import { useEffect, useRef, useState } from "react"
+
+export interface DemoCodeProps {
+    children: React.ReactNode
+    language?: string
+}
+
+export default function DemoCode({ children, language = "html" }: DemoCodeProps) {
+    const containerRef = useRef<HTMLDivElement>(null)
+    const [markup, setMarkup] = useState<string>("")
+
+    useEffect(() => {
+        if (containerRef.current) {
+            setMarkup(
+                prettier.format(containerRef.current.innerHTML, {
+                    parser: "html",
+                    plugins: [parserHtml],
+                    tabWidth: 4,
+                    useTabs: true,
+                    singleQuote: false
+                })
+            )
+        }
+    }, [children])
+
+    return (
+        <>
+            <div ref={containerRef}>{children}</div>
+
+            <SyntaxHighlighter style={syntaxHighlighterStyle} language={language}>
+                {markup}
+            </SyntaxHighlighter>
+        </>
+    )
+}
