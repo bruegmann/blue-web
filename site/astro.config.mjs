3644790fd6b7480bd2ab01f28755f75b69d207f4
lgk-bsw <lgk@patorg.de>
2025-04-02 15:22:58 +0200

    Refactor menu component and styles: update site URL, remove 404.html, and enhance collapsible sections for CSS and JavaScript menus

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -6,7 +6,7 @@ import remarkRender from "./src/plugins/remark-render.mjs"
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://bruegmann.github.io/blue-web/vnext",
+    site: "https://bruegmann.github.io/blue-web/vnext/",
     base: "/blue-web/vnext/",
     integrations: [react(), mdx()],
     vite: {
