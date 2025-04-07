b77c18da84550b5b17af6bc3728a4ae60f8c6e67
lgk-bsw <lgk@patorg.de>
2025-04-07 16:21:58 +0200

    Updates

---
+++ vnext/_astro/ThemeGenerator.DllnA1_G.js
@@ -0,0 +1,1 @@
+import{T as m}from"./ThemeGenerator.CCAFsP83.js";import"./index.Do79TBs0.js";import"./index.F28aNuxU.js";export{m as ThemeGeneratorSwitch};
---
+++ vnext/_astro/sp-theme-Qp-4EUYy.Y84iEGLl.js
@@ -0,0 +1,1 @@
+import{g as e}from"./Theme-qrVzk1Xg.kctr2KJG.js";import"./version-6Oe8yWxl.1hTqD9og.js";customElements.define("sp-theme",e);
