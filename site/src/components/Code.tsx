fc540db89c6a44eb241f88ca889b4cc36496218f
lgk-bsw <lgk@patorg.de>
2025-05-12 12:48:51 +0200

    Simplification in SCSS, better Dark Mode support & new Grab

--- site/src/components/Code.tsx
+++ site/src/components/Code.tsx
@@ -1,20 +1,40 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { synthwave84 as syntaxHighlighterStyle } from "react-syntax-highlighter/dist/esm/styles/prism"
 import CopyToClipboard from "./CopyToClipboard"
+import { useState } from "react"
 
 export interface CodeProps {
     code: string
     language?: string
+    expandable?: boolean
 }
 
-export default function Code({ code, language = "html" }: CodeProps) {
+export default function Code({ code, language = "html", expandable = false }: CodeProps) {
+    const [expanded, setExpanded] = useState(false)
+
     return (
         <div className="position-relative">
             <CopyToClipboard content={code} className="position-absolute top-0 end-0 mt-1 me-1 btn-sm" />
 
-            <SyntaxHighlighter style={syntaxHighlighterStyle} language={language}>
-                {code}
-            </SyntaxHighlighter>
+            <div
+                className="overflow-auto rounded-3"
+                style={{ maxHeight: expandable && !expanded ? "200px" : "400px", transition: "max-height .2s" }}
+            >
+                <SyntaxHighlighter style={syntaxHighlighterStyle} language={language}>
+                    {code}
+                </SyntaxHighlighter>
+            </div>
+
+            {expandable && (
+                <div
+                    className={`position-absolute bottom-0 start-0 end-0 d-flex justify-content-center align-items-center ${expanded ? "py-1" : "top-0 py-3"} rounded-bottom`}
+                    style={{ backgroundImage: "linear-gradient(transparent, black)" }}
+                >
+                    <button type="button" className="btn btn-dark" onClick={() => setExpanded(!expanded)}>
+                        {expanded ? "Collapse" : "Expand"}
+                    </button>
+                </div>
+            )}
         </div>
     )
 }
---
+++ site/src/components/ColorModeSwitch.tsx
@@ -0,0 +1,107 @@
+import { createElement, useEffect, useRef, useState } from "react"
+import { Check, CircleHalf, type Icon, MoonStarsFill, SunFill } from "react-bootstrap-icons"
+// import { Dropdown, DropdownMenu, DropdownToggle } from "reactstrap"
+import { Dropdown } from "bootstrap"
+import { Outside } from "blue-react"
+import { getPreferred } from "@/src/js/color-mode"
+
+function Item({
+    theme,
+    setTheme,
+    thisTheme,
+    icon
+}: {
+    theme: string
+    setTheme: (theme: string) => void
+    thisTheme: string
+    icon: Icon
+}) {
+    return (
+        <button
+            type="button"
+            className={`dropdown-item d-flex align-items-center ${theme === thisTheme ? "active" : ""}`}
+            onClick={() => {
+                setTheme(thisTheme)
+            }}
+        >
+            {createElement(icon, { className: "me-2 opacity-50" })}
+            {thisTheme.charAt(0).toUpperCase() + thisTheme.slice(1)}{" "}
+            {theme === thisTheme && <Check className="bi ms-auto" />}
+        </button>
+    )
+}
+
+export default function ColorModeSwitch() {
+    const [open, setOpen] = useState(false)
+    const toggle = () => {
+        setOpen(!open)
+    }
+    const [theme, setTheme] = useState(getPreferred())
+    const dropdownRef = useRef(null)
+
+    useEffect(() => {
+        if (theme === "auto" && window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            document.documentElement.setAttribute("data-bs-theme", "dark")
+        } else {
+            document.documentElement.setAttribute("data-bs-theme", theme)
+        }
+        localStorage.setItem("blue-web-color-mode", theme)
+        setOpen(false)
+    }, [theme])
+
+    const onMatchMediaChange = () => {
+        localStorage.removeItem("blue-web-color-mode")
+        setTheme(getPreferred())
+    }
+
+    useEffect(() => {
+        window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", onMatchMediaChange)
+
+        return () => {
+            window.matchMedia("(prefers-color-scheme: dark)").removeEventListener("change", onMatchMediaChange)
+        }
+    }, [])
+
+    useEffect(() => {
+        const myElement = dropdownRef.current as unknown as Element
+        let bsDropdown = Dropdown.getInstance(myElement)
+
+        if (!bsDropdown) {
+            bsDropdown = new Dropdown(myElement)
+            bsDropdown.hide()
+            setOpen(false)
+        } else {
+            open ? bsDropdown.show() : bsDropdown.hide()
+        }
+    }, [open])
+
+    const itemProps = { theme, setTheme }
+
+    return (
+        <Outside onClickOutside={() => setOpen(false)}>
+            <div className="dropdown">
+                <button
+                    className="btn blue-btn-soft-secondary dropdown-toggle"
+                    type="button"
+                    onClick={toggle}
+                    ref={dropdownRef}
+                >
+                    {createElement(theme === "light" ? SunFill : theme === "dark" ? MoonStarsFill : CircleHalf, {
+                        className: "bi"
+                    })}
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end" style={{ minWidth: "5rem" }}>
+                    <li>
+                        <Item thisTheme="light" {...itemProps} icon={SunFill} />
+                    </li>
+                    <li>
+                        <Item thisTheme="dark" {...itemProps} icon={MoonStarsFill} />
+                    </li>
+                    <li>
+                        <Item thisTheme="auto" {...itemProps} icon={CircleHalf} />
+                    </li>
+                </ul>
+            </div>
+        </Outside>
+    )
+}
